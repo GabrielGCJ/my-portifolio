07d0475e4f1376025630594520fd49270ffd7504
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-02-05 22:48:46 -0300

    feature/add-initial-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Routes";
+
 export const App = () => {
-
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>App</h1>
-    </div>
-  )
-}
-
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
