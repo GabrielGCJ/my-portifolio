d2eb7fbcd810a0dacbd14b32661204a9bd4703b3
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-02-17 23:18:02 -0300

    feature/add-HeaderContainerRigthMobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MenuLanguage } from "../MenuLanguage";
 import { MenuAboutMe } from "../MenuAboutMe";
+import icone from "../../assents/icone-options.svg";
 import {
   HeaderContainer,
   HeaderContainerRigth,
   GlobalHeaderContainer,
-  HeaderContainerLeft
+  HeaderContainerLeft,
+  HeaderContainerRigthMobile,
 } from "./styles";
 
 export const Header = () => {
   const [languageContainerActive, setLanguageContainerActive] = useState(false);
   const [aboutMeContainerActive, setAboutMeContainerActive] = useState(false);
+  const [widthScream, setWidthScream] = useState(window.innerWidth);
+  const larguraLimite = 700;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidthScream(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [widthScream]);
 
   return (
     <GlobalHeaderContainer>
@@ -18,33 +34,40 @@ export const Header = () => {
         <HeaderContainerLeft>
           <h1>Gabriel Jorge</h1>
         </HeaderContainerLeft>
-        <HeaderContainerRigth>
-          <h1>Home</h1>
-          <div
-            onMouseEnter={() => setLanguageContainerActive(true)}
-            onMouseLeave={() => setLanguageContainerActive(false)}
-          >
-            <h1
-            // onMouseOver ={toggleLanguage}
+
+        {widthScream >= larguraLimite ? (
+          <HeaderContainerRigth>
+            <h1>Home</h1>
+            <div
+              onMouseEnter={() => setLanguageContainerActive(true)}
+              onMouseLeave={() => setLanguageContainerActive(false)}
             >
-              Idioma
-            </h1>
-            {languageContainerActive && <MenuLanguage />}
-          </div>
-          <div
-            onMouseEnter={() => setAboutMeContainerActive(true)}
-            onMouseLeave={() => setAboutMeContainerActive(false)}
-          >
-            <h1
-            // onMouseOver ={toggleLanguage}
+              <h1
+              // onMouseOver ={toggleLanguage}
+              >
+                Idioma
+              </h1>
+              {languageContainerActive && <MenuLanguage />}
+            </div>
+            <div
+              onMouseEnter={() => setAboutMeContainerActive(true)}
+              onMouseLeave={() => setAboutMeContainerActive(false)}
             >
-              Quem sou eu
-            </h1>
-            {aboutMeContainerActive && <MenuAboutMe />}
-          </div>
-          <h1>Projetos</h1>
-          <h1>Contato</h1>
-        </HeaderContainerRigth>
+              <h1
+              // onMouseOver ={toggleLanguage}
+              >
+                Quem sou eu
+              </h1>
+              {aboutMeContainerActive && <MenuAboutMe />}
+            </div>
+            <h1>Projetos</h1>
+            <h1>Contato</h1>
+          </HeaderContainerRigth>
+        ) : (
+          <HeaderContainerRigthMobile>
+            <img src={icone} alt="" style={{ fill: "white" }} />
+          </HeaderContainerRigthMobile>
+        )}
       </HeaderContainer>
     </GlobalHeaderContainer>
   );
