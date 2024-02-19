cf3993f77ad901d45532875b9adcb211c2c324be
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-02-19 18:37:50 -0300

    feature/add-footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+display: flex ;
+flex-direction: row ;
+gap: 1vw;
+justify-content: center;
+background-color: black;
+color: white ;
+font-size: .700rem ;
+
+
+.negrito{
+    font-weight: bold;
+}
+
+`
