cba1f73fbafe7ce61ad20534ccc0ebb58d5203b6
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-02-28 22:34:34 -0300

    feature/testw

--- src/components/PresentationComponent/styles.ts
+++ src/components/PresentationComponent/styles.ts
@@ -56,7 +56,7 @@ export const WallpaperLayerContainer = styled.div`
   height: 100vh;
   width: 100%;
 
-  background: url(${img});
+  background-image: url(${img});
   transform: rotate(180deg);
   background-size: 400%;
   background-position: center;
