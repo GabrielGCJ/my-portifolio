7201d794069595530572a6882e9952d1f85cda81
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-04-04 15:25:35 -0300

    feature/remove-timer-loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { LateralMenu } from "../../components/LateralMenu";
 import { PresentationComponent } from "../../components/PresentationComponent";
 import { WhoAmI } from "../../components/WhoAmI";
@@ -9,19 +9,8 @@ import { SliderComponent } from "../../components/SliderComponent";
 import { Contact } from "../../components/Contact";
 
 export function HomePage() {
-  const [loading, setLoading] = useState(true);
   const { menuMobileOptions, widthScream } = useContext(PortfolioContext);
   const larguraLimite = 750;
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
 
   return (
     <HomeContainer>
