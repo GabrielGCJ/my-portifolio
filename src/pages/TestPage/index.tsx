2c4dca3f8b4ec08026c8c40537de814906f6a06d
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-07-22 23:53:13 -0300

    feature/clean

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -1,12 +1,9 @@
-// import { WhatDoIDo } from "../../components/WhatDoIDo"
-
 import { Contact } from "../../components/Contact"
 import { TestContainer } from "./styles"
 
 export const TestPage = () => {
     return(
         <TestContainer>
-            {/* <WhatDoIDo/> */}
             <Contact/>
         </TestContainer>
     )
