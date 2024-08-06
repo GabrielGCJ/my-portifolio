ba7b6a28d6c782183c329cce1b742ef3725ae650
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-08-05 23:39:34 -0300

    feature/update/WhatDoIDo

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { HomePage } from "./pages/Home";
-import { TestPage } from "./pages/TestPage";
+// import { TestPage } from "./pages/TestPage";
+import { WhatDoIDo } from "./components/WhatDoIDo";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={<TestPage />} />
+        <Route path="/test" element={<WhatDoIDo />} />
       </Route>
     </Routes>
   );
