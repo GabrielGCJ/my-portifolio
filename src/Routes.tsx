0dff1e7e55f2e48d3b1e9c054c158838fc897a94
Gabriel Jorge <gabriel.ggcj@gmail.com>
2026-02-28 14:28:40 -0300

    feature/add-error-page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { HomePage } from "./pages/Home";
-// import { TestPage } from "./pages/TestPage";
 import { WhatDoIDo } from "./components/WhatDoIDo";
+import { ErrorPage } from "./pages/ErrorPage";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={<WhatDoIDo />} />
+        <Route index element={<HomePage />} />
+        <Route path="test" element={<WhatDoIDo />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
