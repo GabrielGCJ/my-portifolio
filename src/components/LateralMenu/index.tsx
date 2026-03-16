4c80b5fee17b7c8bee9d332967bfbed69de86d17
Gabriel Jorge <gabriel.ggcj@gmail.com>
2026-03-16 13:58:47 -0300

    feature/remove-icons

--- src/components/LateralMenu/index.tsx
+++ src/components/LateralMenu/index.tsx
@@ -1,9 +1,9 @@
 import { LateralMenuContainer, LateralMenuContainerGlobal } from "./styles";
 import gitImage from "../../assents/lateralMenu/github.svg";
-import instagramImage from "../../assents/lateralMenu/instagram.svg";
+// import instagramImage from "../../assents/lateralMenu/instagram.svg";
 import linkedinImage from "../../assents/lateralMenu/linkedin.svg";
 import whatsappImage from "../../assents/lateralMenu/whatsapp.svg";
-import spotifyImage from "../../assents/lateralMenu/spotify.svg";
+// import spotifyImage from "../../assents/lateralMenu/spotify.svg";
 
 export const LateralMenu = () => {
   return (
@@ -13,9 +13,9 @@ export const LateralMenu = () => {
           <img className="whiteIcon" src={gitImage} alt="" />
         </a>
 
-        <a href="https://www.instagram.com/gb.j_dev/" target="_blank">
+        {/* <a href="https://www.instagram.com/gb.j_dev/" target="_blank">
           <img className="whiteIcon" src={instagramImage} alt="" />
-        </a>
+        </a> */}
         <a
           href="https://www.linkedin.com/in/gabriel-jorge-67635b221/"
           target="_blank"
@@ -25,12 +25,12 @@ export const LateralMenu = () => {
         <a href="https://wa.me/5521988436194" target="_blank">
           <img className="whiteIcon" src={whatsappImage} alt="" />
         </a>
-        <a
+        {/* <a
           href="https://open.spotify.com/user/ggcj.333?si=j5p0rC6TRpyt3OA3AtgHjQ&utm_source=copy-link"
           target="_blank"
         >
           <img className="whiteIcon" src={spotifyImage} alt="" />
-        </a>
+        </a> */}
       </LateralMenuContainer>
     </LateralMenuContainerGlobal>
   );
